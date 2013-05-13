a375aa45aa0cb162cecd33c7fec6724163211717
Repetilo Andrey <raptor1053@yandex.ru>
2013-05-13 14:04:27 +0300

    changes for deploy

--- testing/settings/base.py
+++ testing/settings/base.py
@@ -120,7 +120,7 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
     )
 
-ROOT_URLCONF = 'urls'
+ROOT_URLCONF = 'testing.urls'
 
 TEMPLATE_DIRS = (
     root('templates'),
@@ -179,3 +179,10 @@ try:
     from utils import *
 except:
     print 'please add utils.py'
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
